4529738a4cb0cd7f617ad1e5b4639edbdfa5801e
right-x2 <kjw970103@gmail.com>
2020-06-03 17:47:10 +0900

    gulp

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,14 @@
+import { handleMessageNotif } from "./chat";
 // eslint-disable-next-line no-undef
 const socket = io("/");
 
 function sendMessage(message) {
   socket.emit("newMessage", { message });
-  console.log(`you: ${message}`);
+  console.log(`You: ${message}`);
 }
 
 function setNickname(nickname) {
   socket.emit("setNickname", { nickname });
-}
-function handleMessageNotif(data) {
-  const { message, nickname } = data;
-  console.log(`${nickname}: ${message}`);
 }
 
 socket.on("messageNotif", handleMessageNotif);
