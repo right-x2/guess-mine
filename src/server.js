85fb460d1e11193f138e44fbf7170f709ccd70d7
right-x2 <kjw970103@gmail.com>
2020-06-03 15:40:54 +0900

    socket programing

--- src/server.js
+++ src/server.js
@@ -1,14 +1,34 @@
 import express from "express";
 import { join } from "path";
-import soketIO from "socket.io";
+import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
+
 app.get("/", (req, res) => res.render("home"));
+
 const handleListening = () =>
   console.log(`server running http://localhost${PORT}`);
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO.listen(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Anon",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,17 @@
-hello;
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`you: ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+function handleMessageNotif(data) {
+  const { message, nickname } = data;
+  console.log(`${nickname}: ${message}`);
+}
+
+socket.on("messageNotif", handleMessageNotif);
